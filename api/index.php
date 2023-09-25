a3a240ab346248c967fcbe320e61d4fcb845bb38
tomasu <tomas.broz1998@gmail.com>
2023-09-25 01:04:31 +0100

    adds basedir

--- api/index.php
+++ api/index.php
@@ -3,39 +3,38 @@
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Content-Type: application/json');
 
+$baseDir = __DIR__; // Get the current directory of the script.
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['endpoint'] === 'products') {
-    $productsFile = '/api/products.json';
-    if(!file_exists($productsFile) && filesize($productsFile) <= 0) exit;
+    $productsFile = $baseDir . '/products.json';
+    if (!file_exists($productsFile) || filesize($productsFile) <= 0) exit;
 
     $data = file_get_contents($productsFile);
     $dataArray = json_decode($data, true);
     $response = $dataArray;
 
-    if(isset($_GET['limit'])){
+    if (isset($_GET['limit'])) {
         $limit = $_GET['limit'];
-        
-        if(isset($_GET['page'])){
+
+        if (isset($_GET['page'])) {
             $page = $_GET['page'];
             $response = array_slice($dataArray, ($page - 1) * $limit, $limit);
-        }
-        else {
+        } else {
             $response = array_slice($dataArray, 0, $limit);
         }
     }
 
     echo json_encode($response);
-} 
-else if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['endpoint'] === 'categories'){
-    $categoriesFile = '/api/categories.json';
-    if(!file_exists($categoriesFile) && filesize($categoriesFile) <= 0) exit;
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['endpoint'] === 'categories') {
+    $categoriesFile = $baseDir . '/categories.json';
+    if (!file_exists($categoriesFile) || filesize($categoriesFile) <= 0) exit;
 
     $data = file_get_contents($categoriesFile);
 
     $dataArray = json_decode($data, true);
     $response = $dataArray;
     echo json_encode($response);
-}
-else {
+} else {
     $response = array('error' => 'Invalid endpoint');
     echo json_encode($response);
 }
